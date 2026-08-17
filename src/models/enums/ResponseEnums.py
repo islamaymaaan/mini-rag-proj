33d8850e030acc067d291ed1557b5deca4e74807
islam ayman <eaboouf55@gmail.com>
2026-08-17 19:36:04 +0300

    wtf_is_this

---
+++ src/models/enums/ResponseEnums.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class ResponseSignal(Enum):
+    FILE_VALIDATED_SUCESS = "file_validated_successfully"
+    FILE_TYPE_NOT_SUPPORTED ="file_type_not_supported"
+    FILE_SIZE_EXCEEDED = "file_size_exceeded"
+    FILE_UPLOAD_SUCESS = "file_upload_sucess"
+    FILE_UPLOAD_FAILED = "file_upload_failed"
