cfba232dfe934ed3c9e25a4280921fc2528b9c5d
islam ayman <eaboouf55@gmail.com>
2026-08-17 14:49:27 +0300

    chages

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
-app=FastAPI()
-@app.get("/welcome")
-def welcome():
-    return {"message": "Welcome to FastAPI!"}
+from dotenv import load_dotenv
+load_dotenv(".env")
+
+from routes import base
+
+app = FastAPI()
+
+app.include_router(base.base_router)
