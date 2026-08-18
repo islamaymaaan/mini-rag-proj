1c6732afca949facbfd8b072a700de59b22a2582
islam ayman <eaboouf55@gmail.com>
2026-08-18 12:03:06 +0300

    returning file id with file path

--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -24,7 +24,7 @@ class DataController(BaseController):
 
         return True, ResponseSignal.FILE_UPLOAD_SUCESS.value
 
-    def generate_unique_filename(self, orig_filename: str, project_id: str):
+    def generate_unique_filepath(self, orig_filename: str, project_id: str):
         # Generate a unique filename using a timestamp or UUID
         random_key = self.generate_random_string() 
         project_path = ProjectController().get_project_path(project_id=project_id) 
@@ -43,7 +43,7 @@ class DataController(BaseController):
                 project_path, f"{random_key}_{cleaned_filename}"
             )
         
-        return new_file_path
+        return new_file_path , f"{random_key}_{cleaned_filename}"
 
 
 
