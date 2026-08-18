0d8f9a803572f2023de9066a6db5ba472133c895
islam ayman <eaboouf55@gmail.com>
2026-08-18 14:14:08 +0300

    file processing and chunks

--- src/models/enums/ResponseEnums.py
+++ src/models/enums/ResponseEnums.py
@@ -6,3 +6,5 @@ class ResponseSignal(Enum):
     FILE_SIZE_EXCEEDED = "file_size_exceeded"
     FILE_UPLOAD_SUCESS = "file_upload_sucess"
     FILE_UPLOAD_FAILED = "file_upload_failed"
+    PROCESSING_SUCESS = "processing_sucess"
+    PROCESSING_FAILED = "processing_failed"
