f4c195d43707006fc88017ba6b20941b51f63084
islam ayman <eaboouf55@gmail.com>
2026-08-19 22:47:05 +0300

    indexing

--- src/routes/data.py
+++ src/routes/data.py
@@ -9,7 +9,10 @@ import logging
 from .schemes.data import ProcessRequest
 from models.ProjectModel import ProjectModel
 from models.ChunkModel import ChunckModel
-from models.db_schemes import DataChunk
+from models.AssetModel import AssetModel
+from models.db_schemes import DataChunk, Asset
+from models.enums.AssetTypeEnum import AssetTypeEnum
+from bson import ObjectId
 
 
 
@@ -61,10 +64,25 @@ async def upload_data(request: Request, project_id: str, file: UploadFile
               }
          )
 
+    #store the assets into the database
+    asset_model= await AssetModel.create_instance(
+         db_client= request.app.db_client
+    )
+
+    asset_resource = Asset(
+         asset_project_id=ObjectId(project.id) if isinstance(project.id, str) else project.id,
+         asset_type= AssetTypeEnum.FILE.value,
+         asset_name= file_id,
+         asset_size= os.path.getsize(file_path)
+
+    )
+
+    asset_record = await asset_model.create_asset(asset= asset_resource)
+
     return JSONResponse(
         content={
             "signal": ResponseSignal.FILE_UPLOAD_SUCESS.value,
-            "file_id": file_id,
+            "file_id": str(asset_record.id),
             
         }
     )
